d888a476e5c6b52df10279bebb8deb679a0a88f8
ld <l.duarte@optologic.ch>
2024-07-03 18:11:41 +0200

    humanize

--- server/settings.py
+++ server/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     # Plugins
     "django_extensions",
     "debug_toolbar",
